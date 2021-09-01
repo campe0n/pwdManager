13552eb3f9c2b95bca5fd31b6cc670f451bc4b91
melvin <melvinrf@Outlook.com>
2021-09-01 14:16:28 -0500

    installed react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
+import React from "react";
+
 import "./App.css";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <header>
-        <h1>Password Manager </h1>
-        <nav>
-          <li>Dashboard</li>
-        </nav>
-      </header>
-    </div>
+    <main>
+      <Router>
+        <header>
+          <h1>Password Manager </h1>
+          <nav>
+            <li>Dashboard</li>
+          </nav>
+        </header>
+      </Router>
+    </main>
   );
 }
 
